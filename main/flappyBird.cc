7c10503e4f0d6ccf43b118ae73867f2657df1eb7
Szymi13 <urbanszymon13@gmail.com>
2023-02-09 00:59:18 +0100

    papap

--- main/flappyBird.cc
+++ main/flappyBird.cc
@@ -5,7 +5,7 @@
 #include <thread>
 
 // checking for operating system for console clearing not sure if it works i
-// didn't test on windows
+// didn't test on windows and can't be bothered
 #ifdef __unix__
 std::string sysClear = "clear";
 
@@ -14,52 +14,53 @@ std::string sysClear = "cls";
 #endif
 
 class Board{
-public:
-	char board_table[64][16] = {{}};
+    public:
+        char board_table[64][16] = {{}};
 
-	void display(){
-		for (int height = 0; height < 16; height++)
-		{
-			for (int width = 0; width < 64; width++)
-			{
-				std::cout << board_table[height][width];
-			}
-			std::cout << std::endl;
-		}
-	}
+        void display(){
+            for (int height = 0; height < 15; height++)
+            {
+                for (int width = 0; width < 63; width++)
+                {
+                    std::cout << board_table[width][height];
+
+                }
+                std::cout << std::endl;
+            }
+        }
 };
 
 int fall(unsigned char y)
 {
-	if (y >= 15)
-	{
-		return 0;
-	}
-	else
-	{
-		y++;
-		return y;
-	}
+    if (y >= 15)
+    {
+        return 0;
+    }
+    else
+    {
+        y++;
+        return y;
+    }
 }
 
 int main(int argc, char **argv)
 {
-	Board board;
-	char bird = 'b';
-	unsigned char birdPosX = 5;
-	unsigned char birdPosY = 5;
-	board.board_table[birdPosY][birdPosX] = bird;
+    Board board;
+    char bird = 'b';
+    unsigned char birdPosX = 5;
+    unsigned char birdPosY = 5;
+    board.board_table[birdPosX][birdPosY] = bird;
 
-	while (1)
-	{
-		system(sysClear.c_str());
-		board.board_table[birdPosY][birdPosX] = ' ';
-		birdPosY = fall(birdPosY);
-		board.board_table[birdPosY][birdPosX] = 'b';
-		board.display();
-		
-		std::this_thread::sleep_for(
-			std::chrono::milliseconds(200)
-		);
-	}
+    while (1)
+    {
+        system(sysClear.c_str());
+        board.board_table[birdPosX][birdPosY] = ' ';
+        birdPosY = fall(birdPosY);
+        board.board_table[birdPosX][birdPosY] = bird;
+        board.display();
+
+        std::this_thread::sleep_for(
+                std::chrono::milliseconds(500)
+                );
+    }
 }
