3d250fc389d130d8b94f6be81109b0b49fb1b145
Szymi13 <urbanszymon13@gmail.com>
2023-02-11 18:16:37 +0100

    obiekt przeszkoda sie wyświetla

--- src/flappyBird.cc
+++ src/flappyBird.cc
@@ -20,10 +20,8 @@ class Board{
         char board_table[64][16] = {{}};
 
         Board(){
-            for (int height = 0; height < 16; height++)
-            {
-                for (int width = 0; width < 64; width++)
-                {
+            for (int height = 0; height < 16; height++){
+                for (int width = 0; width < 64; width++){
                     if(height == 15){
                         board_table[width][height] = *"x"; 
                     }else{
@@ -34,41 +32,49 @@ class Board{
         }
 
         void display(){
-            for (int height = 0; height < 16; height++)
-            {
-                for (int width = 0; width < 64; width++)
-                {
+            for (int height = 0; height < 16; height++){
+                for (int width = 0; width < 64; width++){
                     std::cout << board_table[width][height];
 
                 }
                 std::cout << std::endl;
             }
         }
+
+        void set_field(int x, int y, char value){
+            board_table[x][y] = value;
+        }
 };
 
-int fall(unsigned char y)
-{
-    if (y == 14)
-    {
+class Obstacle{
+    public: 
+    void make_obstacle(Board* board_table){
+        for(int i = 15 - 1; i > 7; i--){
+            board_table -> set_field(46, i, *"O");
+        }
+    }
+};
+
+int fall(unsigned char y){
+    if (y == 14){
         return 0;
     }
-    else
-    {
+    else{
         y++;
         return y;
     }
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
     Board board;
+    Obstacle obstacle;
+    obstacle.make_obstacle(&board);
     char bird = 'b';
     unsigned char birdPosX = 5;
     unsigned char birdPosY = 5;
     board.board_table[birdPosX][birdPosY] = bird;
 
-    while (1)
-    {
+    while (1){
         system(sysClear.c_str());
         board.board_table[birdPosX][birdPosY] = ' ';
         birdPosY = fall(birdPosY);
